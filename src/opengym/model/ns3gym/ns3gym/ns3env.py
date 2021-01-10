c913e506e2bd9cccd0c8d283d72788c8528e526c
Sangwoo Moon <swmoon00@gmail.com>
2021-01-10 16:27:19 +0900

    change

--- src/opengym/model/ns3gym/ns3gym/ns3env.py
+++ src/opengym/model/ns3gym/ns3gym/ns3env.py
@@ -98,7 +98,7 @@ class Ns3ZmqBridge(object):
                     os.kill(self.wafPid, signal.SIGTERM)
                     self.wafPid = None
         except Exception as e:
-            pass
+            print("exception at close", e)
 
     def _create_space(self, spaceDesc):
         space = None

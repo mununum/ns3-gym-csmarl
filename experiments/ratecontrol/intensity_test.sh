5aef51ad1e152c8a74cf3d157e4cf444abd0c923
Sangwoo Moon <swmoon00@gmail.com>
2021-05-18 20:50:16 +0900

    rename folder

--- experiments/ratecontrol/intensity_test.sh
+++ experiments/ratecontrol/intensity_test.sh
@@ -2,19 +2,19 @@
 
 # ndcf test
 
-mkdir -p ndcf_intensity_test
+mkdir -p ndcf_intensity_result
 
 for graphSeed in {0..4}
 do
     python ns3_rollout.py --loss=geometric --topology=10,0.3 --graphSeed=$graphSeed --intensity \
-    > ndcf_intensity_test/$graphSeed.out &
+    > ndcf_intensity_result/$graphSeed.out &
 done
 wait
 
 # odcf test
 
-rm -rf odcf_intensity_test
-mkdir -p odcf_intensity_test
+rm -rf odcf_intensity_result
+mkdir -p odcf_intensity_result
 
 for intensity in $(seq 0.1 0.1 1)
 do
@@ -22,15 +22,15 @@ do
     do
         LD_LIBRARY_PATH=../../build/lib ../../build/scratch/csmarl_test/csmarl_test \
         --loss=geometric --topology=10,0.3 --graphSeed=$graphSeed --intensity=$intensity \
-        --debug=true --algorithm=odcf >> odcf_intensity_test/$graphSeed.out &
+        --debug=true --algorithm=odcf >> odcf_intensity_result/$graphSeed.out &
     done
     wait
 done
 
 # 80211 test
 
-rm -rf 80211_intensity_test
-mkdir -p 80211_intensity_test
+rm -rf 80211_intensity_result
+mkdir -p 80211_intensity_result
 
 for intensity in $(seq 0.1 0.1 1)
 do
@@ -38,7 +38,7 @@ do
     do
         LD_LIBRARY_PATH=../../build/lib ../../build/scratch/csmarl_test/csmarl_test \
         --loss=geometric --topology=10,0.3 --graphSeed=$graphSeed --intensity=$intensity \
-        --debug=true --algorithm=80211 >> 80211_intensity_test/$graphSeed.out &
+        --debug=true --algorithm=80211 >> 80211_intensity_result/$graphSeed.out &
     done
     wait
 done
